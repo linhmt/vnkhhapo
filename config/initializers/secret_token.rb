94bfd67354bb823c24e33173ffc3c5a545940809
VNKHHAPO <linhmt@vietnamairlines.com>
2013-03-29 14:36:05 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vnkhhapo::Application.config.secret_token = '24ebdf83c5d89465ee1708aab9027637dd93730f7b9b8e2c1eb9756c5571a5e46c39fdf5cafa1e64a114d3e40c0609d4a449c8a448e8236d7a4b4f01f4145889'
